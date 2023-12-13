41c65bbbb337a65dc50b8245e18033eab5a2657d
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-13 13:26:51 +0100

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
+void bubble_sort(int *array, size_t size)
+{
+    size_t i, j;
+    int temp;
+
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                print_array(array, size);
+            }
+        }
+    }
+}
