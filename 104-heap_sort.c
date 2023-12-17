69234f48188807a12cf8065cb408467b811bf3e4
Alaa Eddine <Alaaelhannaoui@proton.me>
2023-12-17 15:05:18 +0100

    betty

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -8,26 +8,27 @@
  */
 void sift_down(int *array, size_t start, size_t end, size_t size)
 {
-    size_t root = start;
-    size_t child;
+	size_t root = start;
+	size_t child;
+	int tmp;
 
-    while (2 * root + 1 <= end)
-    {
-        child = 2 * root + 1;
-        if (child + 1 <= end && array[child] < array[child + 1])
-            child++;
+	while (2 * root + 1 <= end)
+	{
+		child = 2 * root + 1;
+		if (child + 1 <= end && array[child] < array[child + 1])
+			child++;
 
-        if (array[root] < array[child])
-        {
-            int temp = array[root];
-            array[root] = array[child];
-            array[child] = temp;
-            root = child;
-            print_array(array, size);
-        }
-        else
-            return;
-    }
+		if (array[root] < array[child])
+		{
+			tmp = array[root];
+			array[root] = array[child];
+			array[child] = tmp;
+			root = child;
+			print_array(array, size);
+		}
+		else
+			return;
+	}
 }
 
 /**
@@ -37,20 +38,20 @@ void sift_down(int *array, size_t start, size_t end, size_t size)
  */
 void heap_sort(int *array, size_t size)
 {
-    int i, tmp;
+	int i, tmp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = size / 2 - 1; i >= 0; i--)
-        sift_down(array, i, size - 1, size);
+	for (i = size / 2 - 1; i >= 0; i--)
+		sift_down(array, i, size - 1, size);
 
-    for (i = size - 1; i > 0; i--)
-    {
-        tmp = array[0];
-        array[0] = array[i];
-        array[i] = tmp;
-        print_array(array, size);
-        sift_down(array, 0, i - 1, size);
-    }
+	for (i = size - 1; i > 0; i--)
+	{
+		tmp = array[0];
+		array[0] = array[i];
+		array[i] = tmp;
+		print_array(array, size);
+		sift_down(array, 0, i - 1, size);
+	}
 }
