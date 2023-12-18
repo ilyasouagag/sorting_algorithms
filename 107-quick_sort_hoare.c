d348b371df418b7250a094ffb55e707acf00ebc9
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-18 23:48:53 +0100

    try

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -12,30 +12,28 @@
  */
 size_t hoare_partition(int *array, size_t start, size_t finish, size_t size)
 {
-	int *pivot, prev, next, tmp;
+    int pivot, above, below;
 
-	pivot = array + start;
-	prev = start - 1;
-	next = finish + 1;
-	while (1)
-	{
-		do {
-			prev++;
-		} while (array[prev] < *pivot);
-		do {
-			next--;
-		} while (array[next] > *pivot);
+    pivot = array[finish];
+    for (above = start - 1, below = finish + 1; above < below;)
+    {
+        do
+        {
+            above++;
+        } while (array[above] < pivot);
+        do
+        {
+            below--;
+        } while (array[below] > pivot);
 
-		if (prev < next)
-		{
-			tmp = array[prev];
-			array[prev] = array[next];
-			array[next] = tmp;
-			print_array(array, size);
-		}
-		else
-			return (next);
-	}
+        if (above < below)
+        {
+            swap_ints(array + above, array + below);
+            print_array(array, size);
+        }
+    }
+
+    return (above);
 }
 
 /**
@@ -48,16 +46,16 @@ size_t hoare_partition(int *array, size_t start, size_t finish, size_t size)
  */
 void quick_sort_helper2(int *array, size_t start, size_t finish, size_t size)
 {
-	size_t pivot;
+    size_t pivot;
 
-	if (start < finish)
-	{
-		pivot = hoare_partition(array, start, finish, size);
+    if (start < finish)
+    {
+        pivot = hoare_partition(array, start, finish, size);
 
-		quick_sort_helper2(array, start, pivot - 1, size);
+        quick_sort_helper2(array, start, pivot - 1, size);
 
-		quick_sort_helper2(array, pivot, finish, size);
-	}
+        quick_sort_helper2(array, pivot, finish, size);
+    }
 }
 
 /**
@@ -67,7 +65,7 @@ void quick_sort_helper2(int *array, size_t start, size_t finish, size_t size)
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
-	quick_sort_helper2(array, 0, size - 1, size);
+    if (array == NULL || size < 2)
+        return;
+    quick_sort_helper2(array, 0, size - 1, size);
 }
