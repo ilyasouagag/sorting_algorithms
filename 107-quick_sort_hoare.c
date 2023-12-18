0c092fc51c01f253f369b54d48ad77d317a78044
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-18 21:08:06 +0100

    betty

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+/**
+ * quick_sort_hoare - sorts an array of integers in ascending order
+ * @array: array input
+ * @size: size of array
+*/
+void quick_sort_hoare(int *array, size_t size)
+{
+
+}
