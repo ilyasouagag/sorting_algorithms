5128701d8bbf997770361cf4e183d0bd82fef386
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-18 23:41:15 +0100

    see now

--- sort.h
+++ sort.h
@@ -44,7 +44,5 @@ void bitonic_mer(int *array, size_t size, size_t start, size_t sq,
 				 char flag);
 void bitonic_seq(int *array, size_t size, size_t start, size_t sq, char flag);
 void bitonic_sort(int *array, size_t size);
-size_t hoare_partition(int *array, size_t start, size_t finish, size_t size);
-void quick_sort_helper2(int *array, size_t start, size_t finish, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 #endif
