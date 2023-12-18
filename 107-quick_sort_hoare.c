947e7dcbfd9f19e2822cfdd6cb68bb2e8c30c5f1
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-18 22:36:28 +0100

    task done

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -74,15 +74,3 @@ void quick_sort(int *array, size_t size)
         return;
     quick_sort_helper(array, 0, size - 1, size);
 }
-int main(void)
-{
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
-
-    print_array(array, n);
-    printf("\n");
-    quick_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
-}
