8bf2bd6c20c834de349809c3572630a4b5cbb129
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-18 23:23:14 +0100

    betty

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -12,9 +12,9 @@
  */
 size_t hoare_partition(int *array, size_t start, size_t finish, size_t size)
 {
-    int pivot, prev, next, tmp;
+    int *pivot, prev, next, tmp;
 
-    pivot = array[finish];
+    pivot = array + start;
     prev = start - 1;
     next = finish + 1;
     while (1)
@@ -22,11 +22,11 @@ size_t hoare_partition(int *array, size_t start, size_t finish, size_t size)
         do
         {
             prev++;
-        } while (array[prev] < pivot);
+        } while (array[prev] < *pivot);
         do
         {
             next--;
-        } while (array[next] > pivot);
+        } while (array[next] > *pivot);
 
         if (prev < next)
         {
