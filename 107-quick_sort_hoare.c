6037cf6ea89642b3cc33755371142c70ba256242
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-18 21:08:45 +0100

    quick sort

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -3,9 +3,8 @@
  * quick_sort_hoare - sorts an array of integers in ascending order
  * @array: array input
  * @size: size of array
- * 
 */
 void quick_sort_hoare(int *array, size_t size)
 {
-
+    
 }
